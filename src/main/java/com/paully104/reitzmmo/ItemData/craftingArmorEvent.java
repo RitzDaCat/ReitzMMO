fba56764309e3c7d3ebd8d9c5f446c67c7e8edc1
Paul Reitz <49771174+PaulAnthonyReitz@users.noreply.github.com>
2020-01-01 22:08:25 -0600

    Update craftingArmorEvent.java

--- src/main/java/com/paully104/reitzmmo/ItemData/craftingArmorEvent.java
+++ src/main/java/com/paully104/reitzmmo/ItemData/craftingArmorEvent.java
@@ -1,18 +1,25 @@
 package com.paully104.reitzmmo.ItemData;
 
+import com.paully104.reitzmmo.ConfigFiles.API;
 import com.paully104.reitzmmo.Custom_Recipes.setItemLore;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Random;
 
 public class craftingArmorEvent implements Listener {
 
+    private final static String LEVEL = "Level";
+
     public boolean isArmour(Material type) {
 
-        if (type.name().contains("boot") || type.name().contains("legging") ||
-                type.name().contains("chestplate") || type.name().contains("helm"))
+        if (type.name().contains("Boot") || type.name().contains("Legging") ||
+                type.name().contains("Chestplate") || type.name().contains("Helmet") || type.name().contains("Cap"))
         {
             return true;
         }
@@ -22,12 +29,27 @@ public class craftingArmorEvent implements Listener {
     }
 
     @EventHandler
+    public void onPlayerCrafting(PrepareItemCraftEvent c)
+    {
+        System.out.println((c.getRecipe()));
+
+    }
+
+    @EventHandler
     public void onPlayerUse(CraftItemEvent event) {
 
         if(isArmour(event.getRecipe().getResult().getType()))
         {
-            nameSpaceKey.setItemDefenseContainer(event.getCurrentItem(),1);
+            Player p = (Player)event.getWhoClicked();
+            int level = API.Players.get(p.getUniqueId().toString()).getData().getInt(LEVEL);
+            int rnd = new Random().nextInt(level);
+            nameSpaceKey.setItemDefenseContainer(event.getRecipe().getResult(),rnd);
+            System.out.print(event.getRecipe().getResult().getType());
 
+        }
+        else
+        {
+            System.out.print("Not armor: " + event.getRecipe().getResult().getType());
         }
 
     }
