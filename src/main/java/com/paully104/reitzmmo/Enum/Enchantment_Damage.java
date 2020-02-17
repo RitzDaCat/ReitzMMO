85d72f3aae12f9963307904d2bb6af1029d7caa3
Paul Reitz <49771174+PaulAnthonyReitz@users.noreply.github.com>
2020-02-16 23:31:28 -0600

    Migration is over
    migration is over

--- src/main/java/com/paully104/reitzmmo/Enum/Enchantment_Damage.java
+++
@@ -1,9 +0,0 @@
-package com.paully104.reitzmmo.Enum;
-
-class Enchantment_Damage {}
-
-
-/* Location:              D:\Minecraft\plugins\ReitzMMO.jar!\com\paully104\reitzmmo\Enum\Enchantment_Damage.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
--- src/main/java/com/paully104/reitzmmo/TODO/ToDo_List.java
+++
@@ -1,9 +0,0 @@
-package com.paully104.reitzmmo.TODO;
-
-class ToDo_List {}
-
-
-/* Location:              D:\Minecraft\plugins\ReitzMMO.jar!\com\paully104\reitzmmo\TODO\ToDo_List.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
